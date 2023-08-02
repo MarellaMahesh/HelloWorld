3a2c982d35ae3cd36ab88661c808f877d40f9b22
MarellaMahesh <51073769+MarellaMahesh@users.noreply.github.com>
2023-08-02 21:39:35 +0530

    Update HelloWorld.java

--- com/main/basic/HelloWorld.java
+++ com/main/basic/HelloWorld.java
@@ -1,1 +1,8 @@
-//This is my first program.
+import java.util.*;
+import java.lang.*;
+
+Class HelloWorld{
+  public static void main(String args[]){
+    System.out.println("This is a sample class.");
+  }
+}
