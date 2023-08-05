e738688dbc7ce2f8bfdd25b8a9092b9e3f3293b8
MarellaMahesh <51073769+MarellaMahesh@users.noreply.github.com>
2023-08-05 19:12:13 +0530

    add 2 numbers.

--- com/main/basic/HelloWorld.java
+++ com/main/basic/HelloWorld.java
@@ -4,5 +4,7 @@ import java.lang.*;
 Class HelloWorld{
   public static void main(String args[]){
     System.out.println("Hi Ramesh, How are you?");
+    int a =10, b=20;
+    System.out.println(a+b);
   }
 }
