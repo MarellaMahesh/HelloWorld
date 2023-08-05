712a5daf88c741d32fa4a1348b8017890d40c5fa
MarellaMahesh <51073769+MarellaMahesh@users.noreply.github.com>
2023-08-05 19:29:15 +0530

    updated method reference

--- com/main/basic/HelloWorld.java
+++ com/main/basic/HelloWorld.java
@@ -5,7 +5,7 @@ Class HelloWorld{
   int num1 = 10, num2 = 20;
   public static void main(String args[]){
     System.out.println("Hi Ramesh, How are you?");
-    System.out.println(a+b);
+    System.out.println(addingTwoNumbers());
   }
 
   private int addingTwoNumbers(){
