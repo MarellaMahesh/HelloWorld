70379c3003930de382859b5bfd426f292f4f3862
MarellaMahesh <51073769+MarellaMahesh@users.noreply.github.com>
2023-08-05 19:42:49 +0530

    Revert "Adding a new message and math util to add 2 numbers. (#2)"
    This reverts commit 08f67dfad820e5efafc8f87ae8ce1b2859dfd5b8.

--- com/main/basic/HelloWorld.java
+++ com/main/basic/HelloWorld.java
@@ -2,13 +2,7 @@ import java.util.*;
 import java.lang.*;
 
 Class HelloWorld{
-  int num1 = 10, num2 = 20;
   public static void main(String args[]){
-    System.out.println("Hi Ramesh, How are you?");
-    System.out.println(addingTwoNumbers());
-  }
-
-  private int addingTwoNumbers(){
-    return num1 + num2;
+    System.out.println("This is a sample class.");
   }
 }
