bfb380f046432a6661c9ddb6acaa58504ca5a7dd
MarellaMahesh <51073769+MarellaMahesh@users.noreply.github.com>
2023-08-05 19:05:48 +0530

    Update console message.

--- com/main/basic/HelloWorld.java
+++ com/main/basic/HelloWorld.java
@@ -3,6 +3,6 @@ import java.lang.*;
 
 Class HelloWorld{
   public static void main(String args[]){
-    System.out.println("This is a sample class.");
+    System.out.println("Hi Ramesh, How are you?");
   }
 }
