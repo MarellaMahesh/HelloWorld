174057e955140335d958188c73b4e979499ff204
MarellaMahesh <51073769+MarellaMahesh@users.noreply.github.com>
2023-08-05 19:27:05 +0530

    Moved local variable to global and added a new method to sum 2 numbers.

--- com/main/basic/HelloWorld.java
+++ com/main/basic/HelloWorld.java
@@ -2,9 +2,13 @@ import java.util.*;
 import java.lang.*;
 
 Class HelloWorld{
+  int num1 = 10, num2 = 20;
   public static void main(String args[]){
     System.out.println("Hi Ramesh, How are you?");
-    int a =10, b=20;
     System.out.println(a+b);
+  }
+
+  private int addingTwoNumbers(){
+    return num1 + num2;
   }
 }
