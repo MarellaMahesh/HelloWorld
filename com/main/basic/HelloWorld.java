08f67dfad820e5efafc8f87ae8ce1b2859dfd5b8
MarellaMahesh <51073769+MarellaMahesh@users.noreply.github.com>
2023-08-05 19:33:39 +0530

    Adding a new message and math util to add 2 numbers. (#2)
    * Update console message.
    
    * add 2 numbers.
    
    * Moved local variable to global and added a new method to sum 2 numbers.
    
    * updated method reference

--- com/main/basic/HelloWorld.java
+++ com/main/basic/HelloWorld.java
@@ -2,7 +2,13 @@ import java.util.*;
 import java.lang.*;
 
 Class HelloWorld{
+  int num1 = 10, num2 = 20;
   public static void main(String args[]){
-    System.out.println("This is a sample class.");
+    System.out.println("Hi Ramesh, How are you?");
+    System.out.println(addingTwoNumbers());
+  }
+
+  private int addingTwoNumbers(){
+    return num1 + num2;
   }
 }
